188a79150dd2ff9157df23edb436c25f9a7c2d1e
Jasper Bollen <Jasperbollen@gmail.com>
2022-03-30 18:14:42 +0200

    Show QRcode on pair

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
-import { BBQDevice } from './ble_ibbq/index'
-import express from 'express'
+import { BBQDevice } from "./ble_ibbq/index";
+import express from "express";
+import qrcode from "qrcode";
 
-const bbqDevice = new BBQDevice('fff0')
-bbqDevice.connect()
+const bbqDevice = new BBQDevice("fff0");
+bbqDevice.on("devicePaired", () => {
+    console.log("Your bluetooth device has been paired. Scan QR code to link device to your account.");
+    qrcode.toString(bbqDevice.id, { type: "terminal" }, function (err, url) {
+        console.log(url);
+    });
+});
+bbqDevice.connect();
 
+const app = express();
 
-const app = express()
+app.get("/api", function (req, res) {
+    res.send("API endpoint");
+});
 
-
-app.get('/api', function (req, res) {
-    res.send('API endpoint')
-})
-
-app.listen(3000)
+app.listen(3000);
